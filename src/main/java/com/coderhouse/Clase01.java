87aecae1c19e7182ccd4f3f15977c19392814c0d
mephi <fmenendezroco@gmail.com>
2025-03-16 20:01:52 -0300

    coderHouse

---
+++ src/main/java/com/coderhouse/Clase01.java
@@ -0,0 +1,96 @@
+package com.coderhouse;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String entrada = "Hola Coders..!!";
+				String entrada = "Hola Coders como estan..!!";
+				//System.out.println(entrada);
+				
+				int miPrimerNumero = 2025;
+				
+				float mumeroConDecimal = 10.55f;
+				
+				// Declarar e inicializar la varaible.
+				int numeroEntero = 13;
+				
+				String mes = "Marzo";
+				
+				// System.out.println(numeroEntero + " de " + mes  + " de " +  miPrimerNumero);
+				
+				//Declarar la Variable
+				String indefinida;		
+				// Inicializo la variabe
+				indefinida = "23233 @ sdfs / sdf4 + + sdfsdw45 - * +  dfgdf ";
+				// System.out.println(indefinida);
+				
+				indefinida = "Nuevo valor";
+				// System.out.println(indefinida);
+				
+				indefinida = "Nuevo valor 2";
+				// System.out.println(indefinida);
+				
+				// Aca vamos a ver una concatenacion
+				// System.out.println(indefinida + mes);
+				
+				// Operaciones matematicas + - * /
+				
+				int suma = 1 + 2;
+				int suma2 = 3 + 5;
+				
+				final int NUMERO_A = 10;
+				final int NUMERO_B = 20;
+				
+				suma = 9;
+				suma2 = 19;
+				//System.out.println(suma + suma2);
+				
+				//System.out.println(NUMERO_A + NUMERO_B);
+				
+//				int contador = 0;		
+//				System.out.println(contador);
+//				contador = 1;
+//				System.out.println(contador);
+//				contador = contador + 1;
+//				System.out.println(contador);
+//				contador = contador + 1;
+//				System.out.println(contador);
+//				contador++;
+//				System.out.println(contador);
+//				contador++;
+//				System.out.println(contador);
+//				
+//				contador = contador - 1;
+//				System.out.println(contador);
+//				contador--;
+//				System.out.println(contador);
+//				contador--;
+//				System.out.println(contador);
+//				contador--;
+//				System.out.println(contador);
+//				contador--;
+//				System.out.println(contador);
+//				contador--;
+//				System.out.println(contador);
+//				contador--;
+//				System.out.println(contador);
+				//asdasd
+				int numeroA = 10;
+				int numeroB = 30;
+				String numeroC = "10";
+				//a
+				// Todas estas comparaciones devuelven un Boolean.
+				System.out.println(numeroA == numeroB); // Igual
+				System.out.println(numeroA != numeroB); // Distinto
+				System.out.println(numeroA < numeroB); // Menor que 
+				System.out.println(numeroA > numeroB); // Mayor que
+				System.out.println(numeroA <= numeroB); // Menor o igual que
+				System.out.println(numeroA >= numeroB); // Mayor o igual que 
+				
+				// Comparacion No permitida por ser FUERTEMENTE TIPADO
+				//System.out.println(numeroA == numeroC);
+		
+	}
+
+}
