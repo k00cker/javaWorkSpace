38a54f3118b233d659a6bdefeb0892fa11a648ae
mephi <fmenendezroco@gmail.com>
2025-04-15 21:57:15 -0400

    FacturacionPrimeraEntregaMenendezv2

---
+++ oriental/src/test/java/com/oriental/OrientalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oriental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
